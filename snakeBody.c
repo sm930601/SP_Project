d8f6a66156a490ed2d7980af8fe7b7237778edb4
DESKTOP-3R2B4GD\YJH <yunjh1994@gmail.com>
2016-12-14 21:21:09 +0900

    complete
    Signed-off-by: DESKTOP-3R2B4GD\YJH <yunjh1994@gmail.com>

--- snakeBody.c
+++ snakeBody.c
@@ -8,27 +8,24 @@ struct SnakeNode {
 	int lastY;
 	struct SnakeNode* link;
 };
-
 typedef struct SnakeNode SnakeNode;
 typedef struct SnakeBody {
 	SnakeNode* head;
 	SnakeNode* tail;
 	int length;
-	int direction;
-	int mdelay;
+	int direction;	//-1 stop, 0 up, 1 right, 2 down, 3 left
+	int mdelay;	//ndelay 마다 direction으로 이동한다.
 	int _mdelay;
 	char ch;
 	int windowX;
 	int windowY;
-	int dead;
-	int redraw;
+	int dead;	//1 dead, 0 live
+	int redraw; //1 need, 0 ignore
 } SnakeBody;
-
 SnakeNode* newSnakeNode(int, int);
 SnakeBody* newSnake(int, int);
 void addSnakeNode(SnakeBody*);
 void stepSnake(SnakeBody*);
-
 SnakeNode* newSnakeNode(int x, int y) {
 	SnakeNode* node = (SnakeNode*)malloc(sizeof(SnakeNode));
 	node->x = x;
@@ -38,7 +35,6 @@ SnakeNode* newSnakeNode(int x, int y) {
 	node->link = NULL;
 	return node;
 }
-
 SnakeBody* newSnake(int x, int y) {
 	SnakeBody* body = (SnakeBody*)malloc(sizeof(SnakeBody));
 	body->ch = 'O';
@@ -48,16 +44,14 @@ SnakeBody* newSnake(int x, int y) {
 	body->windowX = 0;
 	body->windowY = 0;
 	body->dead = 0;
-	body->direction = 0;
-	body->mdelay = 1000;
+	body->direction = -1;
+	body->mdelay = 700;
 	body->_mdelay = 0;
 	body->redraw = 1;
 
 	return body;
 }
-
-void addSnakeNode(SnakeBody* body)
-{
+void addSnakeNode(SnakeBody* body) {	//끝에 노드를 더한다.
 	SnakeNode* tail = body->tail;
 	SnakeNode* add_node = newSnakeNode(tail->lastX, tail->lastY);
 	tail->link = add_node;
@@ -66,10 +60,8 @@ void addSnakeNode(SnakeBody* body)
 
 	body->redraw = 1;
 }
-
-void stepSnake(SnakeBody* body)
-{
-	if(body->direction == -1)
+void stepSnake(SnakeBody* body) {
+	if (body->direction == -1)
 		return;
 
 	body->redraw = 1;
@@ -77,9 +69,8 @@ void stepSnake(SnakeBody* body)
 	SnakeNode* head = body->head;
 	SnakeNode* temp = head;
 	head->lastX = head->x;
-	head->lastY = head->y;
-	
-	while(temp->link) {
+  head->lastY = head->y;
+	while (temp->link) {
 		temp->link->lastX = temp->link->x;
 		temp->link->lastY = temp->link->y;
 		temp->link->x = temp->lastX;
@@ -87,49 +78,46 @@ void stepSnake(SnakeBody* body)
 		temp = temp->link;
 	}
 
-	if(body->direction == 0)
+	if (body->direction == 0)
 		head->y--;
-	else if(body->direction == 1)
+	else if (body->direction == 1)
 		head->x++;
-	else if(body->direction == 2)
+	else if (body->direction == 2)
 		head->y++;
-	else if(body->direction == 3)
+	else if (body->direction == 3)
 		head->x--;
 
 	temp = head->link;
-
-	while(temp) {
-		if(temp->x == head->x && temp->y == head->y) {
+	while (temp) {
+		if (temp->x == head->x && temp->y == head->y) {
 			body->dead = 1;
 			break;
 		}
 		temp = temp->link;
 	}
 }
-
-void snakeRender(SnakeBody* body, int milli)
-{
+void snakeRender(SnakeBody* body, int milli) {
 	int* _mdelay = &(body->_mdelay);
 	*_mdelay += milli;
-
-	if(*_mdelay > body->mdelay) {
+	if (*_mdelay > body->mdelay) {
 		*_mdelay = 0;
 		stepSnake(body);
 	}
-	if(body->redraw == 1) {
+
+	if (body->redraw == 1) {
 		SnakeNode* node = body->head;
-		while(node) {
-			move(body->windowY + node->lastY, body->windowX + node->lastX);
-			addch(' ');
-			node = node->link;
-		}
+		while (node) {
+      move(body->windowY + node->lastY, body->windowX + node->lastX);
+      addch(' ');
+      node = node->link;
+    }
 		node = body->head;
-		while(node) {
+		while (node) {
 			move(body->windowY + node->y, body->windowX + node->x);
 			addch(body->ch);
 			node = node->link;
 		}
 		body->redraw = 0;
 	}
-	move(1, 1);
+	move(1,1);
 }
--- snakeField.c
+++ snakeField.c
@@ -3,12 +3,12 @@
 #include <time.h>
 
 #include "snakeBody.c"
+#include "rank.c"
 
 typedef struct {
 	int x;
 	int y;
 } Item;
-
 typedef struct {
 	int x;
 	int y;
@@ -16,8 +16,9 @@ typedef struct {
 	int height;
 	SnakeBody* snake;
 	Item* item;
-	int gameover;
-	int redraw;
+	int gameover;	//0 is playing, 1 is game over
+	Rank* rank;
+	int redraw; //1 need redraw, 0 ignore
 } SnakeField;
 
 SnakeField* newSnakeField(int x, int y, int width, int height) {
@@ -32,74 +33,84 @@ SnakeField* newSnakeField(int x, int y, int width, int height) {
 	field->snake = newSnake(width / 2, height / 2);
 	field->snake->windowX = x;
 	field->snake->windowY = y;
+	field->rank = NULL;
 
 	makeItemRandomly(field);
 
 	srand((int)time(NULL));
-
 	return field;
 }
-
 void makeItemRandomly(SnakeField* field) {
-	int x = rand() % (field->width - 1) +1;
-	int y = rand() % (field->height -1) +1;
-	int flag = 0;
-
+	int x = rand() % (field->width - 1) + 1;
+	int y = rand() % (field->height - 1) + 1;
+	int flag = 0;	//if item overlap snake, 1
 	do {
 		x = rand() % (field->width - 1) + 1;
 		y = rand() % (field->height - 1) + 1;
 		SnakeNode* node = field->snake->head;
-		while(node) {
-			if(node->x == x && node->y == y)
+		while (node) {
+			if (node->x == x && node->y == y)
 				flag = 1;
 			node = node->link;
 		}
-	} while(flag == 1);
+	} while (flag == 1);
 
 	Item* item = (Item*)malloc(sizeof(Item));
 	item->x = x;
 	item->y = y;
 	field->item = item;
 }
+int snakeFieldKeyInput(SnakeField* field, char key) {
+	if (field->rank != NULL)
+		return rankKeyInput(field->rank, key);
 
-void snakeFieldKeyInput(SnakeField* field, char key) {
 	SnakeNode* head = field->snake->head;
-
-	switch(key) {
-		case 65:
-		case 'w':
-			if(!head->link || head->y == head->link->y)
-				field->snake->direction = 0;
+  switch(key) {
+		case 'q':
+			return -1;
+		case '\n':
+			if (field->gameover == 1)
+				field->rank = createRank(field->snake->length);
 			break;
-		case 66:
-		case 's':
-			if(!head->link || head->y == head->link->y)
-				field->snake->direction = 2;
-			break;
-		case 67:
-		case 'd':
-			if(!head->link || head->x == head->link->x)
-				field->snake->direction = 1;
-			break;
-		case 68:
-		case 'a':
-			if(!head->link || head->x == head->link->x)
-				field->snake->direction = 3;
-			break;
-	}
+    case 65:
+    case 'w':
+			if (!head->link || head->y == head->link->y)
+      	field->snake->direction = 0;
+      break;
+    case 66:
+    case 's':
+			if (!head->link || head->y == head->link->y)
+        field->snake->direction = 2;
+      break;
+    case 67:
+    case 'd':
+			if (!head->link || head->x == head->link->x)
+        field->snake->direction = 1;
+      break;
+    case 68:
+    case 'a':
+			if (!head->link || head->x == head->link->x)
+      	field->snake->direction = 3;
+      break;
+  }
+	return 0;
 }
-
 void snakeFieldRender(SnakeField* field, int milli) {
-	if(field->redraw ==1)
+	if (field->rank != NULL)
 	{
-		for(int i=0 ; i<field->width+2 ; i++) {
+		rankRender(field->rank, milli);
+		return;
+	}
+
+	if (field->redraw == 1)
+	{
+		for(int i=0;i<field->width+2;i++) {
 			move(field->y, field->x+i);
 			addch('X');
 			move(field->y+field->height+1, field->x+i);
 			addch('X');
 		}
-
-		for(int j=0 ; j<field->height+2 ; j++) {
+		for(int j=0;j<field->height+2;j++) {
 			move(field->y+j, field->x);
 			addch('X');
 			move(field->y+j, field->x+field->width+1);
@@ -107,48 +118,46 @@ void snakeFieldRender(SnakeField* field, int milli) {
 		}
 		field->redraw = 0;
 	}
-
 	move(field->y, field->x+field->width+5);
-	addstr("			     ");
+	addstr("                             ");
 	move(field->y, field->x+field->width+5);
 	addstr("length: ");
 	static char textLength[3];
 	sprintf(&textLength, "%d", field->snake->length);
 	addstr(textLength);
 
-	if(field->gameover != 1) {
+	if (field->gameover != 1) {
 		snakeRender(field->snake, milli);
 		SnakeNode* head = field->snake->head;
 
-		if(head->x <= 0 || head->x > field->width)
+		if (head->x <= 0 || head->x > field->width)
 			field->snake->dead = 1;
-		if(head->y <= 0 || head->y > field->height)
-		        field->snake->dead = 1;
-
-		if(field->snake->dead == 1)
+		if (head->y <= 0 || head->y > field->height)
+			field->snake->dead = 1;
+		if (field->snake->dead == 1)
 			field->gameover = 1;
-		if(field->item) {
-			Item* item = field->item;
 
-			if(item->x == head->x && item->y == head->y) {
+		if (field->item) {
+			Item* item = field->item;
+			if (item->x == head->x && item->y == head->y) {
 				addSnakeNode(field->snake);
 				field->snake->mdelay *= 0.8;
 				free(item);
 				field->item = NULL;
-	
+
 				snakeRender(field->snake, 0);
 				makeItemRandomly(field);
-				}
-		}	
+			}
+		}
 
-		if(field->item) {
+		if (field->item) {
 			move(field->y+field->item->y, field->x+field->item->x);
-			addch('|');
+			addch('I');
 		}
 	}
 	else {
-		move(field->y+field->height/2, field->x+field->width/2);
-		addstr("game over");
+		move(field->y+field->height/2,field->x+field->width/2);
+		addstr("game over press enter to view rank");
 	}
-	move(0, 0);
+	move(0,0);
 }
