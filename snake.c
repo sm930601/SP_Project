7d3f082b3441ff6d2a4debe2302db667055f692f
Seong Min <sm930601@naver.com>
2016-12-15 23:28:06 +0900

    snake

--- snake.c
+++ snake.c
@@ -7,67 +7,47 @@
 #include "snakeField.c"
 
 void tty_mode(int);
-void set_nodelay_mode();
 void set_noecho_mode();
-void render(int);
+void set_nodelay_mode();
+void render(int);	//milliseconds
 SnakeField* field;
-
-main()
-{
+main() {
 	initscr();
 
-	tty_mode(0);
+	tty_mode(0);		//상태 저장
+	//set_noecho_mode();
 	noecho();
-	set_nodelay_mode();
+	set_nodelay_mode();	//입력을 받기위해서 입력 딜레이를 없앤다.
 
-	field = newSnakeField(5, 1, 40, 20);
+	field = newSnakeField(5,1,40,20);	//실제 게임이 돌아가는 필드
 
 	while(true) {
 		//clear();
-		render(1000/60);
+		render(1000/30);	//30fps
 		refresh();
-		usleep(1000000/60);
+		usleep(1000000/30);
 		char c = getch();
-		if(c == 'q') {
-			break;
-		}
-		snakeFieldKeyInput(field, c);
+		if (snakeFieldKeyInput(field, c) < 0)
+      break;
 	}
 
-	tty_mode(1);
+	tty_mode(1);		//상태 복원
 	endwin();
 }
-
-void render(int milli) 
-{
+void render(int milli) {
 	snakeFieldRender(field, milli);
 }
-
-void tty_mode(int how)
-{
+void tty_mode(int how) {
 	static struct termios original_mode;
 	static int original_flags;
-
-	if(how == 0) {
+	if (how == 0) {
 		tcgetattr(0, &original_mode);
 		original_flags = fcntl(0, F_GETFL);
-	}
-	else
-	{
+	} else {
 		tcsetattr(0, TCSANOW, &original_mode);
 		fcntl(0, F_SETFL, original_flags);
 	}
 }
-
-void set_nodelay_mode()
-{
-	int termflags;
-	termflags = fcntl(0, F_GETFL);
-	termflags |= O_NDELAY;
-	fcntl(0, F_SETFL, termflags);
-}
-
-
 void set_noecho_mode() {
 	struct termios ttystate;
 	tcgetattr(0, &ttystate);
@@ -76,4 +56,9 @@ void set_noecho_mode() {
 	ttystate.c_cc[VTIME] = 0;
 	tcsetattr(0, TCSANOW, &ttystate);
 }
-
+void set_nodelay_mode() {
+	int termflags;
+	termflags = fcntl(0, F_GETFL);
+	termflags |= O_NDELAY;
+	fcntl(0, F_SETFL, termflags);
+}
